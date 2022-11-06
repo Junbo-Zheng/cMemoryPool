93a64aae0147df816042a0e87793a17cefe27f60
Junbo Zheng <zhengjunbo1@xiaomi.com>
2022-11-06 18:35:29 +0800

    fix build warning
    Signed-off-by: Junbo Zheng <zhengjunbo1@xiaomi.com>

--- src/debug.h
+++ src/debug.h
@@ -1,8 +1,20 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #ifndef __PACKED
 #define __PACKED __attribute__((packed))
 #endif
+
+void init_tracer(void);
+
+bool del_a_tracer_record(void* malloc_ptr, char* file_name, uint32_t func_line);
+
+char* get_filename(char* path);
+
+bool add_a_tracer_record(uint8_t memx, uint16_t mem_sz, void* malloc_ptr,
+                         char* file_name, uint32_t func_line);
 
 #endif /* _DEBUG_H_ */
